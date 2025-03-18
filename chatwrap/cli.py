34eec3cea8d5601e70af123f0875f7a3fa71d26d
Adela Rus <adela.rus.ext@servicepro.de>
2025-03-18 15:23:07 +0200

    cleanup

--- chatwrap/cli.py
+++ chatwrap/cli.py
@@ -1,27 +1,27 @@
 #Command line interface from ChatWrap
 
 from argparse import ArgumentParser
+import os
 from chatwrap.client import LLMClient
+from dotenv import load_dotenv
 
-LLM_SERVER_URL = 'http://127.0.0.1:1234/v1'
+LLM_SERVER_URL = 'https://api.openai.com/v1/chat/completions'	
+
 
 def cli(params): 
-    print(params.prompt)
-    
-    llmClient = LLMClient(LLM_SERVER_URL)
+    load_dotenv()
+
+    llmClient = LLMClient(LLM_SERVER_URL, os.getenv('OPENAI_API_KEY'))
     
     llmClient.send_request(params.prompt, params.model, params.temperature, params.streaming)
-    llmClient.send_request(params.prompt, params.model, params.temperature, params.streaming)
-
-    print(f'Total number {llmClient.totalNoOfTokens}')
-
+  
 if __name__== "__main__":
 
     args = ArgumentParser('Chat Wrap')
     
     args.add_argument('prompt', help='prompt to use')
     args.add_argument('--version', action='version', version='%(prog)s 0.1')
-    args.add_argument('--model', help='model to use', default='qwen2.5-coder-0.5b-instruct')
+    args.add_argument('--model', help='model to use', default='gpt-4o')
     args.add_argument('--temperature', help='temperature for sampling', default=0.7)
     args.add_argument('--streaming', action='store_true', help='streaming mode')
 
--- chatwrap/client.py
+++ chatwrap/client.py
@@ -27,7 +27,6 @@ class LLMClient:
     Connect to LLM sever
     send request to LLM server
     '''
-
     totalNoOfTokens = 0
     
     def __init__(self, url, openai_key):
@@ -35,48 +34,11 @@ class LLMClient:
 
         self.url = url
 
-        print(f'Connecting to {self.url}')
-
-        response = requests.get(f'{self.url}/models')
-
-        if response.status_code == 200:
-            print('Connected to LLM')
-
-            models = response.json()
-
-            #print(f'Available models: {models}')
-
-
-    def log_tokens(funct):
-        def wrapper(*args, **kwargs):
-           
-            result = funct(*args, **kwargs)
-  
-            total_tokens = result.get('usage', {}).get('total_tokens', 0)
-
-            LLMClient.totalNoOfTokens += total_tokens
-
-            print(f'Total tokens used: {total_tokens}')
- 
-            return result
-
-        return wrapper
-
-    #@log_tokens
     @cache_request
-    def send_request(self, prompt, model="4o-mini", temperature=0.7, streaming=False,
+    def send_request(self, prompt, model="gpt-4o", temperature=0.7, streaming=False,
                      system_prompt='You are a helpful HR assistant'):
-        # body = {
-        #     "model": model,
-        #     "messages": [
-        #     { "role": "system", "content": "" },
-        #     { "role": "user", "content": prompt }
-        #     ],
-        #     "temperature": temperature,
-        #     "max_tokens": 100,
-        #     "stream": streaming
-        # }
-        body = {"model": "gpt-4o",
+       
+        body = {"model": model,
                "messages": [
                     {"role": "system", "content": system_prompt},
                     {"role": "user", "content":prompt}
@@ -87,14 +49,13 @@ class LLMClient:
         }
        
         response = requests.post(f'{self.url}', json=body, headers=headers)
-
+      
         if response.status_code == 200:
             response_json = response.json()
             message_content = response_json.get('choices', [{}])[0].get('message', {}).get('content', 'No content')
             
-            print(f"Response*: {message_content}")
-           
+            print(f"Response: {message_content}")
+            return json.loads(message_content)         
         else:
             print(f'Error: {response.status_code}')
-    
-        return json.loads(message_content)
+        
